92457aa7a98260a59ece3a3a47cc858598fb8908
Frank V. Castellucci <frankiecast@gmail.com>
2022-05-05 05:15:42 -0400

    Structured keypair for pasta

--- cli/src/pasta_keys.rs
+++ cli/src/pasta_keys.rs
@@ -1,26 +1,58 @@
+use std::fmt;
+
 use pasta_curves::group::ff::Field;
+use pasta_curves::group::ff::PrimeField;
 use pasta_curves::group::Group;
 use pasta_curves::group::GroupEncoding;
 use pasta_curves::{pallas::Scalar, Ep};
+use rand::rngs::OsRng;
+use rand::CryptoRng;
 use rand::RngCore;
 
 /// Fqp is a pallas::Scalar
 pub type Fqp = Scalar;
 
-#[derive(Debug, Clone, Copy)]
-pub struct PastaSecretKey(pub Fqp);
+#[derive(Clone, Copy)]
+pub struct PastaKeyPair(pub Fqp);
+impl PastaKeyPair {
+    /// Constructs a new, random `Keypair` using a caller-proveded RNG
+    fn generate<R>(csprng: &mut R) -> Self
+    where
+        R: CryptoRng + RngCore,
+    {
+        Self(Fqp::random(csprng))
+    }
 
-impl PastaSecretKey {
-    pub fn random(mut rng: impl RngCore) -> Self {
-        Self(Fqp::random(&mut rng))
+    /// Constructs a new, random `Keypair` using `OsRng`
+    pub fn new() -> Self {
+        let mut rng = OsRng::default();
+        Self::generate(&mut rng)
+    }
+
+    pub fn secret(&self) -> Fqp {
+        self.0
+    }
+
+    pub fn public_key(&self) -> PastaPublicKey {
+        PastaPublicKey(Ep::generator() * self.0)
+    }
+}
+
+/// For debugging but we need to get to the 64 bytes?
+impl fmt::Debug for PastaKeyPair {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let tmp = self.0.to_repr();
+        write!(f, "{}", bs58::encode(tmp).into_string())?;
+        Ok(())
     }
 }
 #[derive(Debug, Clone, Copy)]
 pub struct PastaPublicKey(pub Ep);
 
 impl PastaPublicKey {
-    pub fn from_secret_key(s: &PastaSecretKey) -> Self {
-        Self(Ep::generator() * s.0)
+    pub fn new_random() -> Self {
+        let mut rng = OsRng::default();
+        PastaKeyPair::generate(&mut rng).public_key()
     }
 
     pub fn to_bytes(&self) -> [u8; 32] {
@@ -37,6 +69,3 @@ impl PastaPublicKey {
         Self(Ep::from_bytes(&bff).unwrap())
     }
 }
-
-#[derive(Debug, Clone, Copy)]
-pub struct PastaKeyPair;
