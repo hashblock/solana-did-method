4d490fe7f543537818c353aa89d7a483710d3dfb
Frank V. Castellucci <frankiecast@gmail.com>
2022-07-17 19:13:20 -0400

    Abstract Chain

--- cli/src/chain_trait.rs
+++ cli/src/chain_trait.rs
@@ -6,8 +6,20 @@ use crate::errors::SolDidResult;
 
 /// DIdSigner is a type able to sign transactions
 pub type DidSigner = Vec<u8>;
+pub type ChainSignature = String;
 pub trait Chain {
-    fn inception_inst(&self, event_msg: &EventMessage<SaidEvent<Event>>) -> SolDidResult<String>;
-    fn rotation_inst_fn(&self, event_msg: &EventMessage<SaidEvent<Event>>) -> SolDidResult<String>;
+    /// Inception instruction put on the chain
+    fn inception_inst(
+        &self,
+        event_msg: &EventMessage<SaidEvent<Event>>,
+    ) -> SolDidResult<ChainSignature>;
+    /// Rotation instruction put on the chain
+    fn rotation_inst_fn(
+        &self,
+        event_msg: &EventMessage<SaidEvent<Event>>,
+    ) -> SolDidResult<ChainSignature>;
+    /// Get the signer bytes
     fn inst_signer(&self) -> DidSigner;
+    /// Get the chain URL in use
+    fn url(&self) -> &String;
 }
--- cli/src/solana_wrap/schain_wrap.rs
+++ cli/src/solana_wrap/schain_wrap.rs
@@ -1,7 +1,7 @@
 //! Solana Chain wraps the interface and behavior for block chain
 
 use crate::{
-    chain_trait::{Chain, DidSigner},
+    chain_trait::{Chain, ChainSignature, DidSigner},
     errors::SolDidResult,
 };
 
@@ -13,39 +13,69 @@ use solana_sdk::{
 };
 
 pub struct SolanaChain {
+    rpc_url: String,
     rpc_client: RpcClient,
     signer: Keypair,
 }
 
-impl SolanaChain {}
+impl SolanaChain {
+    /// Create a new chain instance with designated client and signer
+    pub fn new(rpc_client: RpcClient, signer: Keypair) -> Self {
+        let rpc_url = rpc_client.url();
+        Self {
+            rpc_url,
+            rpc_client,
+            signer,
+        }
+    }
+    /// Get the version of the chain node
+    pub async fn version(&self) -> semver::Version {
+        let version = self.rpc_client.get_version().await.unwrap();
+        semver::Version::parse(&version.solana_core).unwrap()
+    }
+}
 
+/// Default implementation for SolanaChain
 impl Default for SolanaChain {
     fn default() -> Self {
         let cli_config = match &*solana_cli_config::CONFIG_FILE {
             Some(cfgpath) => solana_cli_config::Config::load(&cfgpath).unwrap(),
             None => solana_cli_config::Config::default(),
         };
+        let rpc_url = cli_config.json_rpc_url.clone();
         let default_signer = read_keypair_file(cli_config.keypair_path).unwrap();
         let rpc_client =
             RpcClient::new_with_commitment(cli_config.json_rpc_url, CommitmentConfig::confirmed());
 
         Self {
             rpc_client: rpc_client,
+            rpc_url,
             signer: default_signer,
         }
     }
 }
 
+/// Chain trait implementation
 impl Chain for SolanaChain {
-    fn inception_inst(&self, event_msg: &EventMessage<SaidEvent<Event>>) -> SolDidResult<String> {
+    fn inception_inst(
+        &self,
+        _event_msg: &EventMessage<SaidEvent<Event>>,
+    ) -> SolDidResult<ChainSignature> {
         todo!()
     }
 
-    fn rotation_inst_fn(&self, event_msg: &EventMessage<SaidEvent<Event>>) -> SolDidResult<String> {
+    fn rotation_inst_fn(
+        &self,
+        _event_msg: &EventMessage<SaidEvent<Event>>,
+    ) -> SolDidResult<ChainSignature> {
         todo!()
     }
 
     fn inst_signer(&self) -> DidSigner {
         self.signer.to_bytes().to_vec()
+    }
+
+    fn url(&self) -> &String {
+        &self.rpc_url
     }
 }
